9239b4f0b653e2c2b7857b4677c175c97a472ecd
Kevin Maillet <maillet.kevin91@gmail.com>
2015-12-26 16:43:02 +0100

    commit

--- hello.py
+++ hello.py
@@ -9,6 +9,25 @@ class Application:
     def __init__(self):
         pass
 
+    @classmethod
+    def preprocess_id(cls, o):
+        if o is None or o == "" or o == "null":
+            return o
+        if isinstance(o, cursor.Cursor):
+            o = json.loads(dumps(o))
+        if isinstance(o, list):
+            for i in range(len(o)):
+                o[i] = Application.preprocess_id(o[i])
+            return o
+        if o["_id"]:
+            if isinstance(o["_id"], ObjectId):
+                o["_id"] = str(o["_id"])
+            else:
+                if isinstance(o["_id"], dict):
+                    o["_id"] = str(o["_id"]["$oid"])
+        return o
+
+
 @Application.app.route('/')
 def home():
     return render_template("test.html")

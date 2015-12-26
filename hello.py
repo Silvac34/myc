1a638d005e78770b5c08db03cacca937b1e4f315
Kevin Maillet <maillet.kevin91@gmail.com>
2015-12-26 16:40:12 +0100

    add Mongodb

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, Response, render_template, request
+from pymongo import MongoClient, cursor
 
 class Application:
     app = Flask(__name__)
+    db = MongoClient('localhost')['Shrt']
 
     def __init__(self):
         pass

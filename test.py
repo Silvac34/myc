a036714c646024c9be807e433187ec543edb4e82
Kevin Maillet <maillet.kevin91@gmail.com>
2015-12-26 16:09:05 +0100

    test

--- test.py
+++ test.py
@@ -9,5 +9,5 @@ def hello():
 
 if __name__ == "__main__":
 #    DevData.populate_database() #to populate the database in development environment
-    Application.app.debug = True
-    Application.app.run(host='0.0.0.0')
+    app.debug = True
+    app.run(host='0.0.0.0')

8297e722433e2d287d11aa9e36822c9107ff4bb6
Kevin Maillet <maillet.kevin91@gmail.com>
2015-12-26 16:08:03 +0100

    teste

--- test.py
+++ test.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template("test.html")
+
+if __name__ == "__main__":
+#    DevData.populate_database() #to populate the database in development environment
+    Application.app.debug = True
+    Application.app.run(host='0.0.0.0')

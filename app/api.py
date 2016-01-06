bcc35507f5f44d5edeeaff45db7c0084e35abe1e
Kevin Maillet <maillet.kevin91@gmail.com>
2016-01-06 16:28:03 +0100

    test heroku mongolab

--- app/api.py
+++ app/api.py
@@ -10,12 +10,13 @@ mongolab_uri= "mongodb://heroku_dnnkp4sr:6s6svorb6cdq990q4i0kaoar1t@ds039185.mon
 
 class Application:
     app = Flask(__name__)
-    client = MongoClient(mongolab_uri,
+    #client = MongoClient(mongolab_uri,
+    db = MongoClient(mongolab_uri,
                          connectTimeoutMS=30000,
                          socketTimeoutMS=None,
-                         socketKeepAlive=True)
+                         socketKeepAlive=True)['Shrt']
 
-    db = client.get_default_database()
+    #db = client.get_default_database()
 #    db = MongoClient('localhost')['Shrt']
 
     def __init__(self):

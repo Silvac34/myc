d462a21a036669d7eeb5648808c9af037cb36f92
Kevin Maillet <maillet.kevin91@gmail.com>
2016-03-14 21:26:57 +0000

    solved little bug facebook auth

--- app/static/app.js
+++ app/static/app.js
@@ -10,58 +10,69 @@ var app = angular.module('myApp', [
   'myApp.viewLogin'
 ]);
 
-app.config(['$stateProvider','$urlRouterProvider','$authProvider',function($stateProvider, $urlRouterProvider,$authProvider) {
+app.config(['$stateProvider', '$urlRouterProvider', '$authProvider', function($stateProvider, $urlRouterProvider, $authProvider) {
+
 
   $stateProvider
-  .state('login', {
-    url: '/login',
-    templateUrl: 'static/viewLogin/viewLogin.html',
-    controller: 'ViewLoginCtrl'
-  })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'static/viewLogin/viewLogin.html',
+      controller: 'ViewLoginCtrl'
+    })
   $stateProvider
-  .state('view_meals', {
-    url: '/view_meals',
-    templateUrl: 'static/viewMeals/viewMeals.html',
-    controller: 'ViewMealsCtrl',
-    data: {requiredLogin: true}
-  })
+    .state('view_meals', {
+      url: '/view_meals',
+      templateUrl: 'static/viewMeals/viewMeals.html',
+      controller: 'ViewMealsCtrl',
+      data: {
+        requiredLogin: true
+      }
+    })
   $stateProvider
-  .state('create_meal', {
-    url: '/create_meal',
-    templateUrl: 'static/viewCreateMeal/viewCreateMeal.html',
-    controller: 'ViewCreateMealCtrl',
-    data: {requiredLogin: true}
-  })
+    .state('create_meal', {
+      url: '/create_meal',
+      templateUrl: 'static/viewCreateMeal/viewCreateMeal.html',
+      controller: 'ViewCreateMealCtrl',
+      data: {
+        requiredLogin: true
+      }
+    })
   $stateProvider
-  .state('my_meals', {
-    url: '/my_meals',
-    templateUrl: 'static/viewMyMeals/viewMyMeals.html',
-    controller: 'ViewMyMealsCtrl',
-    data: {requiredLogin: true}
-  })
+    .state('my_meals', {
+      url: '/my_meals',
+      templateUrl: 'static/viewMyMeals/viewMyMeals.html',
+      controller: 'ViewMyMealsCtrl',
+      data: {
+        requiredLogin: true
+      }
+    })
   $urlRouterProvider.otherwise('view_meals');
 
   $authProvider.facebook({
-      clientId: '1533480140278594',
-      // by default, the redirect URI is http://localhost:5000
-      redirectUri: 'https://ide.c9.io/kevin_maillet/shareat'
-    });
+    clientId: '1533480140278594',
+    // by default, the redirect URI is http://localhost:5000
+    redirectUri: 'https://shareat-kevin-maillet.c9users.io/'
+  });
+
+
+
+
 
 }]);
 
 
-app.run(function ($rootScope, $state, $auth) {
+app.run(function($rootScope, $state, $auth) {
   $rootScope.$on('$stateChangeStart',
-  function (event, toState) {
-    var requiredLogin = false;
-    // check if this state need login
-    if (toState.data && toState.data.requiredLogin)
-    requiredLogin = true;
+    function(event, toState) {
+      var requiredLogin = false;
+      // check if this state need login
+      if (toState.data && toState.data.requiredLogin)
+        requiredLogin = true;
 
-    // if yes and if this user is not logged in, redirect him to login page
-    if (requiredLogin && !$auth.isAuthenticated()) {
-      event.preventDefault();
-      $state.go('login');
-    }
-  });
+      // if yes and if this user is not logged in, redirect him to login page
+      if (requiredLogin && !$auth.isAuthenticated()) {
+        event.preventDefault();
+        $state.go('login');
+      }
+    });
 });

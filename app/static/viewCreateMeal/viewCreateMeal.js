918dd96e02948cb4d7e5d0c079117a0499322ed2
Kevin Maillet <maillet.kevin91@gmail.com>
2016-03-28 15:49:17 +0000

    resolved scope issue create meal

--- app/static/viewCreateMeal/viewCreateMeal.js
+++ app/static/viewCreateMeal/viewCreateMeal.js
@@ -7,66 +7,110 @@ angular.module('myApp.viewCreateMeal', ['ui.router'])
   $urlRouterProvider.when('/create_meal', '/create_meal/diner');
   $stateProvider
     .state('create_meal.diner', {
+      parent: 'create_meal',
       url: '/diner',
-      templateUrl: 'static/viewCreateMeal/viewCreateMealDiner/viewCreateMealDiner.html'
+      templateUrl: 'static/viewCreateMeal/viewCreateMealDiner/viewCreateMealDiner.html',
+      controller: 'ViewCreateMealCtrl'
     })
   $stateProvider
     .state('create_meal.profile', {
+      parent: 'create_meal',
       url: '/profile',
-      templateUrl: 'static/viewCreateMeal/viewCreateMealProfile/viewCreateMealProfile.html'
+      templateUrl: 'static/viewCreateMeal/viewCreateMealProfile/viewCreateMealProfile.html',
+      controller: 'ViewCreateMealCtrl'
     })
   $stateProvider
     .state('create_meal.payment', {
+      parent: 'create_meal',
       url: '/payment',
-      templateUrl: 'static/viewCreateMeal/viewCreateMealPayment/viewCreateMealPayment.html'
+      templateUrl: 'static/viewCreateMeal/viewCreateMealPayment/viewCreateMealPayment.html',
+      controller: 'ViewCreateMealCtrl'
     });
 }])
 
 .controller('ViewCreateMealCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $scope.createMeal = function(meal) {
-      $http.post('/api/meal', meal);
+  //initialize the editedMeal model
+  $scope.editedMeal = $scope.editedMeal || {
+    veggies: false,
+    town: "Santiago",
+    requiredHelpers:[]
+  };
+
+  //initialize the buyers model
+  $scope.buyers = $scope.buyers || {
+      time: ""
+    },
+
+    $scope.cooks = $scope.cooks || {
+      nbCooks: ""
+    },
+
+    $scope.cleaners = $scope.cleaners || {
+      nbCleaners: ""
     },
 
 
-    $scope.checkboxHelpType = {
-      buying: false,
-      cooking: false,
-      cleaning: false,
-      notHelping: false
+    //initialize
+    $scope.checkboxHelpType = $scope.checkboxHelpType || {
+      helpBuying: false,
+      helpCooking: false,
+      helpCleaning: false,
+      notHelping: true
 
     },
 
-    $scope.excludingHelp = function() {
-      $scope.checkboxHelpType.buying = false,
-        $scope.checkboxHelpType.cooking = false,
-        $scope.checkboxHelpType.cleaning = false
-    },
+
 
     $scope.includingHelp = function() {
       $scope.checkboxHelpType.notHelping = false
     }
 
+  $scope.excludingHelp = function() {
+    $scope.checkboxHelpType.helpBuying = false,
+      $scope.checkboxHelpType.helpCooking = false,
+      $scope.checkboxHelpType.helpCleaning = false
+  }
+
+
+
+  $scope.createMeal = function() {
+    if ($scope.checkboxHelpType.helpBuying == true) {
+      //$scope.editedMeal.requiredHelpers.buyers = $scope.buyers
+      $scope.editedMeal.requiredHelpers.push({"buyers":$scope.buyers})
+    }
+    if ($scope.checkboxHelpType.helpCooking == true) {
+      $scope.editedMeal.requiredHelpers.push({"cooks":$scope.cooks})
+    }
+    if ($scope.checkboxHelpType.helpCleaning == true) {
+      $scope.editedMeal.requiredHelpers.push({"cleaners":$scope.cleaners})
+    }
+    $http.post('/api/meal', meal);
+    
+    //TODO : rediriger vers page du repas
+  }
+
+
   ////ngrepeat for the cooking question
-  $scope.nbCookers = [{
-    number_cooker: "0",
-    default_option: true
-  }, {
-    number_cooker: "1",
-    default_option: false
-  }, {
-    number_cooker: "2",
-    default_option: false
-  }, {
-    number_cooker: "3",
-    default_option: false
-  }, {
-    number_cooker: "4",
-    default_option: false
-  }, {
-    number_cooker: "5",
-    default_option: false
-  }];
+  /*  $scope.nbCookers = [{
+      number_cooker: "0",
+      default_option: true
+    }, {
+      number_cooker: "1",
+      default_option: false
+    }, {
+      number_cooker: "2",
+      default_option: false
+    }, {
+      number_cooker: "3",
+      default_option: false
+    }, {
+      number_cooker: "4",
+      default_option: false
+    }, {
+      number_cooker: "5",
+      default_option: false
+    }];
   //ngrepeat for the cleaning question
   $scope.nbCleaners = [{
     number_cleaner: "0",
@@ -86,6 +130,6 @@ angular.module('myApp.viewCreateMeal', ['ui.router'])
   }, {
     number_cleaner: "5",
     default_option: false
-  }];
+  }];*/
 
 }]);

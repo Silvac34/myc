ba7cef5182fb4a11d32c2d920419889ecb06d1b3
Kevin Maillet <maillet.kevin91@gmail.com>
2016-04-07 21:55:35 +0000

    deleted config from git

--- app/static/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-// THIS FILE IS AUTO-GENERATED! EDITS               WILL NOT PERSIST!
-// Make edits in the appropriate JSON            file under ./config
-angular.module('config', [])
-
-.constant('ENV', {fbClientID:'1555866924706582',fbRedirectURI:'https://shareat-dimitrikohn.c9users.io/'})
-
-;

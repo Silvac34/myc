b5de086fa1ff9bda9f7cf43312137870467950e0
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-04-11 17:09:14 +0000

    buton go back ok, need to solve problemas with child views

--- app/static/viewCreateMeal/viewCreateMeal.js
+++ app/static/viewCreateMeal/viewCreateMeal.js
@@ -52,14 +52,12 @@ angular.module('myApp.viewCreateMeal', ['ui.router'])
     last_page: false
   },
   
-  $scope.change_animation_type = function() {
-      $scope.animation.is_animated = true;
-      $scope.animation.is_not_animated = false;
-  },
   
   $scope.change_animation_to_the_right = function() {
       $scope.animation.next_page = true;
       $scope.animation.last_page = false;
+      $scope.animation.is_animated = true;
+      $scope.animation.is_not_animated = false;
   },
   
   $scope.change_animation_to_the_left = function() {

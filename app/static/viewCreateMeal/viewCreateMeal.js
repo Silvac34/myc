dc6155e052a9989fa2c72e106e64d41e810ec06b
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-04-23 21:18:24 +0000

    Calendar for form

--- app/static/viewCreateMeal/viewCreateMeal.js
+++ app/static/viewCreateMeal/viewCreateMeal.js
@@ -109,6 +109,33 @@ angular.module('myApp.viewCreateMeal', ['ui.router','ngAnimate'])
     //TODO : rediriger vers page du repas
   }
 
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  }
+  
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+  
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+  
+  $scope.popup1 = {
+    opened: false
+  };
+  
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.altInputFormats = ['M!/d!/yyyy'];
 
 }])
 

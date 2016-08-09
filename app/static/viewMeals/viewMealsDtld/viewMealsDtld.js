d417ebcb50361f2134cd1ae9764b1069cf7f4eed
Kevin Maillet <maillet.kevin91@gmail.com>
2016-08-09 21:15:28 +0000

    Card 49 started work on Detailed Meal

---
+++ app/static/viewMeals/viewMealsDtld/viewMealsDtld.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var modMealsDetailed = angular.module('myApp.viewMealsDtld', ['angular-svg-round-progressbar','ui.bootstrap'])
+
+
+.controller('ViewMealsDtldCtrl', ['$scope','$http','meal_id',function($scope,$http,meal_id) {
+  
+  $scope.loadMealInfo = function(meal_id) {
+    $http.get('/api/meal/' + meal_id,meal_id).success(function(data) {
+      $scope.meal = data;
+    });
+  }
+  
+  $scope.loadMealInfo(meal_id)
+  
+}]);
+

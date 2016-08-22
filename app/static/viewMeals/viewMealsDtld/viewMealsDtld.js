8dfcc43fcb86526ebd503e2b972f24fff6d986a1
Kevin Maillet <maillet.kevin91@gmail.com>
2016-08-22 16:25:40 +0000

    Card 77 https://trello.com/c/OlrTtndH front end meal subscription

--- app/static/viewMeals/viewMealsDtld/viewMealsDtld.js
+++ app/static/viewMeals/viewMealsDtld/viewMealsDtld.js
@@ -6,29 +6,66 @@ var modMealsDetailed = angular.module('myApp.viewMealsDtld', ['angular-svg-round
 .controller('ViewMealsDtldCtrl', ['$scope', '$http', 'meal_id', function($scope, $http, meal_id) {
 
   $scope.loadMealInfo = function(meal_id) {
-    $http.get('/api/meal/' + meal_id, meal_id).then(function(response) {
+    $http.get('/api/meal/' + meal_id).then(function(response) {
       $scope.meal = response.data;
-      
+
       /*to check wether there is available space for each rôle*/
-      if (!$scope.meal.detailedInfo.requiredGuests.cooks||$scope.meal.detailedInfo.requiredGuests.cooks.nbRemainingPlaces <= 0) {
+      if (!$scope.meal.detailedInfo.requiredGuests.cooks || $scope.meal.detailedInfo.requiredGuests.cooks.nbRemainingPlaces <= 0) {
         $scope.requiredGuests.availablePlaces['cooks'] = false;
       }
-      else $scope.requiredGuests.availablePlaces['cooks'] = true
-      if (!$scope.meal.detailedInfo.requiredGuests.cleaners||$scope.meal.detailedInfo.requiredGuests.cleaners.nbRemainingPlaces <= 0) {
+      else {
+        $scope.requiredGuests.availablePlaces['cooks'] = true;
+        $scope.requestRole.name = "cook";
+      }
+      if (!$scope.meal.detailedInfo.requiredGuests.cleaners || $scope.meal.detailedInfo.requiredGuests.cleaners.nbRemainingPlaces <= 0) {
         $scope.requiredGuests.availablePlaces['cleaners'] = false;
       }
-      else $scope.requiredGuests.availablePlaces['cleaners'] = true;
-      if (!$scope.meal.detailedInfo.requiredGuests.simpleGuests||$scope.meal.detailedInfo.requiredGuests.simpleGuests.nbRemainingPlaces <= 0) {
+      else {
+        $scope.requiredGuests.availablePlaces['cleaners'] = true;
+        if (!$scope.requestRole) {
+          $scope.requestRole.name = "cleaner"
+        }
+      }
+      if (!$scope.meal.detailedInfo.requiredGuests.simpleGuests || $scope.meal.detailedInfo.requiredGuests.simpleGuests.nbRemainingPlaces <= 0) {
         $scope.requiredGuests.availablePlaces['simpleGuests'] = false;
       }
-      else $scope.requiredGuests.availablePlaces['simpleGuests'] = true;
+      else {
+        $scope.requiredGuests.availablePlaces['simpleGuests'] = true;
+        if (!$scope.requestRole) {
+          $scope.requestRole.name = "simpleGuest"
+        }
+      }
     });
   }
-
-  $scope.requiredGuests={availablePlaces:{}} /*to initialize the variable */
+  
+  
+  $scope.subscribeMeal = function(meal_id,role) {
+    $http.post('/api/meal/' + meal_id +'/subscription', {"requestRole":role}).then(function(response) {
+      //$scope.loadMealInfo(meal_id);
+      $scope.meal.nbRemainingPlaces -=  1 
+      $scope.meal.detailedInfo.requiredGuests[$scope.requestRole.name + "s"].nbRemainingPlaces -= 1 
+      $scope.goToMeal = true;
+    }, function(response){
+      $scope.loadMealInfo(meal_id);
+      $scope.errorSubscribe.status = true
+      $scope.errorSubscribe.message = response.data
+    })
+  }
+  
+  //Initialize variable
+  $scope.requestRole={}; 
+  $scope.requiredGuests = {
+    availablePlaces: {}
+  } 
+  $scope.errorSubscribe = {"status":false};
+  $scope.goToMeal = false;
   
   $scope.loadMealInfo(meal_id);
-
   $scope.accordionOneAtATime = true;
+  
+  $scope.closeAlert = function(){
+    $scope.errorSubscribe.status = false
+  }
+  
 
 }]);

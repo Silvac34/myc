511c4a093676e9750f34a739880b42982383d42a
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-09-01 07:05:28 +0000

    https://trello.com/c/rnapXjSy ajout du premier cadre cuisinier avec les infos

--- app/static/viewMyMeals/viewMyMeals.js
+++ app/static/viewMyMeals/viewMyMeals.js
@@ -9,7 +9,7 @@ angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar
         url: '/my_meals/:myMealId',
         templateUrl: 'static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.html',
         controller: 'ViewMyMealsDtldCtrl'
-      })
+      });
 }])
 
 .controller('ViewMyMealsCtrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {

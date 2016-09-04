ff5c96267ca81551ff3b289ac94e28bac11bc4a3
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-09-04 09:00:23 +0000

    Refactorisation de viewMyMeals.js, création du fichier viewMyMealsDtld.js

--- app/static/viewMyMeals/viewMyMeals.js
+++ app/static/viewMyMeals/viewMyMeals.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap'])
+angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap','myApp.viewMyMealsDtld'])
 
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
@@ -22,92 +22,4 @@ angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar
 
     $scope.loadMeals();
 
-  $scope.updateMeal = function(meal) {
-    $http.put('/api/meal/' + meal._id, meal);
-  };
-
-  $scope.deleteMeal = function(meal_id) {
-    $http.delete('/api/meal/' + meal_id).then(function() {
-      for (var i in $scope.meals) {
-        if ($scope.meals[i]._id == meal_id) {
-          $scope.meals.splice(i, 1);
-          break;
-          alert($scope.meals[i].label)
-        }
-      }
-    });
-  };
-
-}])
-
-.controller('ViewMyMealsDtldCtrl', ['$scope', '$http', '$stateParams', '$uibModal', function($scope, $http, $stateParams, $uibModal) {
-
-  $scope.loadMyMealInfo = function(meal_id) {
-    $http.get('/api/meal/' + meal_id + '/private').then(function(response) {
-      $scope.meal = response.data;
-      var userId = $scope.user._id;
-
-      for (var i = 0; i < $scope.meal.privateInfo.users.length; i++) {
-        if ($scope.meal.privateInfo.users[i]._id == userId) {
-          if ($scope.meal.privateInfo.users[i].role[0] == "admin") {
-            $scope.userRole = $scope.meal.privateInfo.users[i].role[1];
-            $scope.userAdmin = true;
-          }
-          else {
-            $scope.userRole = $scope.meal.privateInfo.users[i].role[0];
-            $scope.userAdmin = false;
-          }
-        }
-      }
-    });
-  };
-
-  $scope.loadMyMealInfo($stateParams.myMealId);
-
-  //modalDelete
-
-  //modal to define the price of the meal and the number of participants
-  $scope.openModalDelete = function() {
-
-    $uibModal.open({
-      animation: true,
-      templateUrl: 'formModalDelete.html',
-      controller: 'FormModalDeleteInstanceCtrl',
-      size: "sm"
-    });
-  };
-
-}])
-
-.filter('MyMealsFiltered', function() {
-  return function(input, current) {
-    input = input || '';
-    var today = new Date();
-    var output = [];
-    for (var i = 0; i < input.length; i++) {
-      var my_meals_date = new Date(input[i].time);
-      if (current) {
-        if (my_meals_date > today) {
-          output.push(input[i]);
-        }
-      }
-      else {
-        if (my_meals_date < today) {
-          output.push(input[i]);
-        }
-      }
-    }
-    return output;
-  };
-})
-
-.controller('FormModalDeleteInstanceCtrl', function($scope, $uibModalInstance) {
-
-  $scope.ok = function() {
-    $uibModalInstance.close();
-  }; //function to validate the modal
-
-  $scope.cancel = function() {
-    $uibModalInstance.dismiss('cancel');
-  }; //funcion to dismiss the modal
-});
+}]);
---
+++ app/static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var modMyMealsDetailed = angular.module('myApp.viewMyMealsDtld', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap']);
+
+modMyMealsDetailed.controller('ViewMyMealsDtldCtrl', ['$scope', '$http', '$stateParams', '$uibModal', function($scope, $http, $stateParams, $uibModal) {
+
+  $scope.loadMyMealInfo = function(meal_id) {
+    $http.get('/api/meal/' + meal_id + '/private').then(function(response) {
+      $scope.meal = response.data;
+      var userId = $scope.user._id;
+
+      for (var i = 0; i < $scope.meal.privateInfo.users.length; i++) {
+        if ($scope.meal.privateInfo.users[i]._id == userId) {
+          if ($scope.meal.privateInfo.users[i].role[0] == "admin") {
+            $scope.userRole = $scope.meal.privateInfo.users[i].role[1];
+            $scope.userAdmin = true;
+          }
+          else {
+            $scope.userRole = $scope.meal.privateInfo.users[i].role[0];
+            $scope.userAdmin = false;
+          }
+        }
+      }
+    });
+  };
+
+  $scope.loadMyMealInfo($stateParams.myMealId);
+
+  //modalDelete to delete a meal
+  $scope.openModalDelete = function() {
+
+    $uibModal.open({
+      animation: true,
+      templateUrl: 'formModalDelete.html',
+      controller: 'FormModalDeleteInstanceCtrl',
+      size: "sm"
+    });
+  };
+
+}]);
+
+modMyMealsDetailed.filter('MyMealsFiltered', function() {
+  return function(input, current) {
+    input = input || '';
+    var today = new Date();
+    var output = [];
+    for (var i = 0; i < input.length; i++) {
+      var my_meals_date = new Date(input[i].time);
+      if (current) {
+        if (my_meals_date > today) {
+          output.push(input[i]);
+        }
+      }
+      else {
+        if (my_meals_date < today) {
+          output.push(input[i]);
+        }
+      }
+    }
+    return output;
+  };
+});
+
+modMyMealsDetailed.controller('FormModalDeleteInstanceCtrl', function($scope, $uibModalInstance) {
+
+  $scope.ok = function() {
+    $uibModalInstance.close();
+  }; //function to validate the modal
+
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  }; //funcion to dismiss the modal
+});

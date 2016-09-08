953039ce1ea7ecbdb90e874f5bbf3c1e930cf1b4
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-09-08 13:36:39 +0000

    https://trello.com/c/rnapXjSy mise en place des modifications sur la structure du html

--- app/static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.js
+++ app/static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.js
@@ -11,14 +11,7 @@ modMyMealsDetailed.controller('ViewMyMealsDtldCtrl', ['$scope', '$http', '$state
 
       for (var i = 0; i < $scope.meal.privateInfo.users.length; i++) {
         if ($scope.meal.privateInfo.users[i]._id == userId) {
-          if ($scope.meal.privateInfo.users[i].role[0] == "admin") {
-            $scope.userRole = $scope.meal.privateInfo.users[i].role[1];
-            $scope.userAdmin = true;
-          }
-          else {
-            $scope.userRole = $scope.meal.privateInfo.users[i].role[0];
-            $scope.userAdmin = false;
-          }
+          $scope.userRole = $scope.meal.privateInfo.users[i].role[0];
         }
       }
     });

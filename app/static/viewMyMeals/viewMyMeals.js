2a55775fa631c146c4667aa0ecb829517746cda9
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-09-09 09:52:32 +0000

    https://trello.com/c/f6dkzjgz refactoring du state(my_dtld_meal) pour pouvoir passer de accéder aux repas depuis viewmeals

--- app/static/viewMyMeals/viewMyMeals.js
+++ app/static/viewMyMeals/viewMyMeals.js
@@ -2,16 +2,6 @@
 
 angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap','myApp.viewMyMealsDtld'])
 
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-    .state('view_my_dtld_meals', {
-      url: '/my_meals/:myMealId',
-      templateUrl: 'static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.html',
-      controller: 'ViewMyMealsDtldCtrl'
-    });
-}])
-
 .controller('ViewMyMealsCtrl', ['$scope', '$http', '$uibModal', function($scope, $http, $uibModal) {
 
   $scope.loadMeals = function() {

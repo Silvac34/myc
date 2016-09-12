5ea02680ab49ad094f1d8f0613431a6787efe810
Kevin Maillet <maillet.kevin91@gmail.com>
2016-09-12 19:31:07 +0000

    #177 https://trello.com/c/o22qpiBX refonte du backend pour passer avec eve framework

--- app/configure.py
+++ app/configure.py
@@ -9,7 +9,7 @@ class Config(object):
 class ProductionConfig(Config):
     DEBUG = False
     #We use MLab as a database as a service
-    MONGOLAB_URI= 'mongodb://shareat:kmaillet230191@ds055782.mlab.com:55782/shareat_prod'
+    MONGO_URI= 'mongodb://shareat:kmaillet230191@ds055782.mlab.com:55782/shareat_prod'
     TOKEN_SECRET= 'kmaillet230191dkohn1003dflklqksdoklc'
     FACEBOOK_SECRET='5c9c6aae00e33c0daec661d2095d0d6a'
 
@@ -17,22 +17,22 @@ class ProductionConfig(Config):
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    MONGOLAB_URI= 'mongodb://shareat:kmaillet230191@ds019498.mlab.com:19498/shareat_stage'
+    MONGO_URI= 'mongodb://shareat:kmaillet230191@ds019498.mlab.com:19498/shareat_stage'
     TOKEN_SECRET= 'kmaillet230191dkohn1003dflklqksdoklc'
     FACEBOOK_SECRET= '4a944b87ba3cec1475019cc8cdd16bed'
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
-    MONGOLAB_URI= 'mongodb://shareat:kmaillet230191@ds055782.mlab.com:55782/shareat-dev_dim'
+    MONGO_URI= 'mongodb://shareat:kmaillet230191@ds055782.mlab.com:55782/shareat-dev_dim'
     TOKEN_SECRET= 'kmaillet230191dkohn1003dflklqksdoklc'
     FACEBOOK_SECRET= '6f748f72ff723b0b9d11997a6b2c0a37'
 
 class DevelopmentKev(Config):
     DEVELOPMENT = True
     DEBUG = True
-    MONGOLAB_URI= 'mongodb://shareat:kmaillet230191@ds019498.mlab.com:19498/shareat_dev'
-    MONGOLAB_URI_TEST = 'mongodb://shareat:kmaillet230191@ds055872.mlab.com:55872/shareat_dev_test'
+    MONGO_URI= 'mongodb://shareat:kmaillet230191@ds019498.mlab.com:19498/shareat_dev'
+    #MONGOLAB_URI_TEST = 'mongodb://shareat:kmaillet230191@ds055872.mlab.com:55872/shareat_dev_test'
     TOKEN_SECRET= 'kmaillet230191dkohn1003dflklqksdoklc'
     FACEBOOK_SECRET= 'f97f2cc3e469c9675b9d5b9f0b57ba21'
 
---
+++ app/settings.py
@@ -0,0 +1,236 @@
+
+RESOURCE_METHODS = ['GET', 'POST']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+HATEOAS=False
+#to enable concurrency control
+IF_MATCH = False
+URL_PREFIX="api"
+DEBUG = True
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+Schema
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+schemaMeals = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'menu': {
+        'type': 'string',
+        'required': True
+    },
+    'veggies': {
+        'type': 'boolean'
+    },
+    'town': {
+        'type': 'string',
+        'allowed':["Santiago"],
+        'required': True
+    },
+    'admin': { #mettre data relation
+        'type': 'objectid'
+    },
+    'addressApprox': {
+        'type': 'string',
+        'required': True
+    },
+    'time': {
+        'type': 'string',
+        'required': True
+    },
+    'price': {
+        'type': 'number',
+        'min': 0,
+        'required': True
+    },
+    'nbGuests': {
+        'type': 'number',
+        'min': 1,
+        'readonly':True
+    },
+    'nbRemainingPlaces': {
+        'type': 'number',
+        'min': 0,
+        'readonly': True
+    },
+    'privateInfo':{
+        'type': 'dict',
+        'required':True,
+        'schema':{
+            'address': {
+                'type':'string',
+                'required':True
+            },
+            'adminPhone':{'type':'string'},
+            'users':{
+                'type':'list',
+                #'required':True,
+                'schema': {
+                    'type':'dict',
+                    'schema':{
+                        '_id':{ #mettre data relation
+                        'type':'objectid',
+                        'required':True
+                        },
+                        'role': {
+                            'type': 'list',
+                        'allowed': ["admin", "cook", "cleaner","simpleGuest"]
+                       }
+                    }
+                }
+            }
+        }
+    },
+    'detailedInfo':{
+        'type': 'dict',
+        'required':True,
+        'schema':{
+            'requiredGuests': {
+                'type':'dict',
+                'schema':{
+                    'cleaners':{
+                        'type':'dict',
+                        'schema':{
+                            'nbRemainingPlaces':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            },
+                            'nbRquCleaners':{
+                                'type':'number',
+                                'min':0
+                            },
+                            'price':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            }                        
+                        }
+                    },
+                    'cooks':{
+                        'type':'dict',
+                        'schema':{
+                            'nbRemainingPlaces':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            },
+                            'nbRquCooks':{
+                                'type':'number',
+                                'min':0
+                            },
+                            'timeCooking':{
+                                'type':'string',
+                                'required':True
+                            },                            
+                            'price':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            }                        
+                        }
+                    },
+                    'simpleGuests':{
+                        'type':'dict',
+                        'schema':{
+                            'nbRemainingPlaces':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            },
+                            'nbRquSimpleGuests':{
+                                'type':'number',
+                                'min':0
+                            },
+                            'price':{
+                                'type':'number',
+                                'min':0,
+                                'readonly':True
+                            }                        
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+schemaUsers = {
+    '_id': {
+        'type': 'objectid',
+        'readonly': True
+    },
+    'first_name': {
+        'type': 'string'
+    },
+    'last_name': {
+        'type': 'string'
+    },
+    'gender': {
+        'type': 'string',
+        'allowed': ["male","female"]
+    },
+    'privateInfo': {
+        'type': 'dict',
+        'schema':{
+            'facebook_id': {
+                'type':'string'
+            },
+            'email':{'type': 'string', 'regex': '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'},
+            'link':{'type': 'string'}
+        }
+    },
+    'picture': {
+        'type': 'dict',
+        'schema':{
+            'data': {
+                'type':'dict',
+                'schema':{
+                    "url":{'type':'string'},
+                    'is_silhouette':{'type':'boolean'}
+                }
+            }
+        }
+    }
+}
+
+
+
+meals = {
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+    'schema': schemaMeals,
+    'item_methods' : ['GET'],
+    'datasource':{
+        'source': 'meals',
+        'projection': {
+            'privateInfo':0
+        }
+    }
+}
+
+privateMeals = {
+    'url':'meals/private',
+    'resource_methods': ['GET'],
+    'schema': schemaMeals,
+    'item_methods' : ['GET','PATCH','DELETE'],
+    'datasource':{
+        'source': 'meals'
+    }
+}
+
+privateUsers = {
+    'url':'users/private',
+    'resource_methods': ['GET'],
+    'schema': schemaUsers,
+    'item_methods' : [],
+    'datasource':{
+        'source': 'users'
+    }
+}
+
+DOMAIN={
+    'meals':meals,
+    'privateMeals':privateMeals,
+    'privateUsers':privateUsers
+}

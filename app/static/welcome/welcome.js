6b6897c9a1fcf5fd44243c2a9eb868afad585e41
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-11-02 09:18:43 +0000

    https://trello.com/c/GVhp9cgM Rajout des schémas pour le fonctionnement sur la landing page, fin de la landing page v1

--- app/static/welcome/welcome.js
+++ app/static/welcome/welcome.js
@@ -2,6 +2,9 @@
 
 angular.module('myApp.welcome', ['ui.router'])
 
-.controller('WelcomeCtrl', ['$scope',function($scope) {
- $scope;
+.controller('WelcomeCtrl', ['$scope', function($scope) {
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
 }]);

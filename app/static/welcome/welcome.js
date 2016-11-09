39e0b12b99c5bff38fc0336914e4a5955f1d6c07
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-11-09 15:51:58 +0000

    Travail sur l'apparition du prénom lorsque déjà registered sur la welcomepage + mise en forme du who we are et concept du footer

--- app/static/welcome/welcome.js
+++ app/static/welcome/welcome.js
@@ -3,8 +3,5 @@
 angular.module('myApp.welcome', ['ui.router'])
 
 .controller('WelcomeCtrl', ['$scope', function($scope) {
-    $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    $scope.active = 0;
-    var slides = $scope.slides = [];
+
 }]);

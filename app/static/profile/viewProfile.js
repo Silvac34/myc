b8d00a7436a3456b10bc1ed382dab355513d3081
DimitriKohn <dimitri.kohn.pro@gmail.com>
2016-11-21 06:03:40 +0000

    rajout de l'autocomplete avec la liste des pays + les drapeaux dans profile

--- app/static/profile/viewProfile.js
+++ app/static/profile/viewProfile.js
@@ -5,11 +5,35 @@ angular.module('myApp.viewProfile', [])
 .controller('ViewProfileCtrl', ['$scope', 'userServices', function($scope, userServices) {
 
   $scope.getUserProfile = function() {
-      userServices.getUserInfo().then(function(data) {
-        $scope.user = data;
-      });
+    userServices.getUserInfo().then(function(data) {
+      $scope.user = data;
+    });
   };
 
   $scope.getUserProfile();
+
+  var optionsFlags = {
+    url: "../static/bower_components/EasyAutocomplete/demo/resources/countries.json",
+
+    getValue: "name",
+
+    list: {
+      match: {
+        enabled: true
+      },
+      maxNumberOfElements: 8
+    },
+
+    template: {
+      type: "custom",
+      method: function(value, item) {
+        return "<span class='flag flag-" + (item.code).toLowerCase() + "' ></span>" + value;
+      }
+    },
+
+    theme: "round"
+  };
+
+  $("#country_of_origin").easyAutocomplete(optionsFlags);
 
 }]);

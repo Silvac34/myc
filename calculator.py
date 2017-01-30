f788bfea142f4aa44b92fa9733c20c63164dba25
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-01-30 09:16:01 +0000

    https://trello.com/c/pffF44WJ fin de la rédaction du calculator

--- calculator.py
+++ calculator.py
@@ -40,7 +40,7 @@ class Calculator:
     def resolve(self, nbRquSimpleGuests, nbRquCooks, nbRquCleaners, priceGroceries):
         """la fonction resolve effectue le calcul et return chacun des prix"""
         priceUnit = math.ceil( 100000 * priceGroceries / (1 + nbRquCleaners + nbRquCooks + nbRquSimpleGuests) ) / 100000
-        coefHelp = 0.5 #ce coefficient (entre 0 et 1) correspond au % de différence de valorisation de l'aide par rapport au cuisinier: si coefHelp = 0.66, alors l'aide obtient un discount de 0.66*alpha*priceUnit alors que l'hôte à alpha*priceUnit de discount
+        
         if nbRquSimpleGuests == 0: #si le nombre d'invité est nul, le prix est le même pour tout le monde : aides comme hôte
             self.hostPrice = priceUnit
             if nbRquCooks != 0:
@@ -49,33 +49,34 @@ class Calculator:
                 self.cleanerPrice = priceUnit
         
         else:
-            alpha = math.ceil( 100000 * 0.25 * nbRquSimpleGuests / ( (1 + coefHelp * (nbRquCleaners + nbRquCooks) ) * priceUnit) ) / 100000 #coefficient 100000 choisit aléatoirement pour avoir une bonne approx de alpha #alpha correspond à la valorisation du cuisto: il aura un discount de alpha*priceUnit
+            coefValorisationSimpleGuest = 0.25 #l'invité paiera coefValorisationSimpleGuest de fois en plus du prix unitaire du repas
+            correlationHostHelp = 1.5 #l'hôte aura un discount de correlationHostHelp fois plus grand que l'aide
+            alpha = (nbRquSimpleGuests * coefValorisationSimpleGuest) / (1 + (nbRquCleaners + nbRquCooks) / correlationHostHelp) #coef de discount de l'hôte: il aura un discount de alpha*unitPrice
+            beta = alpha / correlationHostHelp
+            alphaZero = (1 + nbRquCleaners + nbRquCooks) / (1 + (nbRquCleaners + nbRquCooks) / correlationHostHelp)
             
-            if alpha <= 1: #cas général de réduction des prix
-                self.hostPrice = priceGroceries
+            if alpha < 1: #cas général de réduction des prix
+                self.hostPrice = (1 - alpha) * priceUnit
                 if nbRquCooks != 0:
-                    self.cookPrice = (1 - alpha * coefHelp) * priceUnit
-                    self.hostPrice -= nbRquCooks * self.cookPrice
+                    self.cookPrice = (1 - beta) * priceUnit
                 if nbRquCleaners != 0:
-                    self.cleanerPrice = (1 - alpha * coefHelp)   * priceUnit
-                    self.hostPrice -= nbRquCleaners * self.cleanerPrice
-                self.simpleGuestPrice = 1.25 * priceUnit
-                self.hostPrice -= nbRquSimpleGuests * self.simpleGuestPrice
+                    self.cleanerPrice = (1 - beta) * priceUnit
+                self.simpleGuestPrice = (1 + coefValorisationSimpleGuest) * priceUnit
                 
-            elif alpha >= 1.5: #cas où le surplus donné par les invités paye intégralement et l'hôte et les aides
+            elif alpha >= alphaZero: #cas où le surplus donné par les invités paye intégralement et l'hôte et les aides
                 self.hostPrice = 0
                 if nbRquCooks != 0:
                     self.cookPrice = 0
                 if nbRquCleaners != 0:
                     self.cleanerPrice = 0
-                self.simpleGuestPrice = 1.25 * priceUnit - (abs((1 - alpha) * priceUnit) + math.ceil(10000*(nbRquCleaners + nbRquCooks) * abs((1 - coefHelp * alpha) * priceUnit)) / nbRquSimpleGuests) / 10000
+                self.simpleGuestPrice = math.ceil(100* priceGroceries / nbRquSimpleGuests)/100
             else: #cas où le surplus donné par les invités paye l'hôte mais pas intégralement les aides, dans ce cas, les aides payent 33% du prix unitaire et les prix invités diminuent
                 self.hostPrice = 0
                 if nbRquCooks != 0:
-                    self.cookPrice = (1 - coefHelp * alpha) * priceUnit
+                    self.cookPrice = ( (1 - beta) - abs((1 - alpha)) / (nbRquCleaners + nbRquCooks) ) * priceUnit
                 if nbRquCleaners != 0:
-                    self.cleanerPrice = (1 - coefHelp * alpha) * priceUnit
-                self.simpleGuestPrice = 1.25 * priceUnit - math.ceil(10000 * (abs((1 - alpha) * priceUnit) / nbRquSimpleGuests)) / 10000
+                    self.cleanerPrice = ( (1 - beta) - abs((1 - alpha)) / (nbRquCleaners + nbRquCooks) ) * priceUnit
+                self.simpleGuestPrice = (1 + coefValorisationSimpleGuest) * priceUnit
          
         #On arrondit tous les prix au dixième
         self.hostPrice = Aproximator(self.hostPrice).numberAproximate
@@ -88,6 +89,6 @@ class Calculator:
         
 monCalcul = Calculator()
 #[nbRquSimpleGuests, nbRquCooks, nbRquCleaners, priceGroceries]
-monResultat = monCalcul.resolve(3,1,1,37)
+monResultat = monCalcul.resolve(23,1,2,60)
 #[hostPrice,cookPrice,cleanerPrice,simpleGuestPrice]
 print(monResultat)

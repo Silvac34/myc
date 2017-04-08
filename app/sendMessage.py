03360523f9f091190a34e3e6f8706c448b28be30
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-04-08 08:38:25 +0000

    https://trello.com/c/AkbN9s7n suppression du module sendMessage.py et remplacement par le service facebookService.js avec dedans la fonction qui récupère les events des users. Pas encore du tout tester

--- app/sendMessage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import requests
-
-token = "EAAVynZAjdOe0BAHR0zDRsqWokHZCbRLjiReLKVJtHgzd4e4mBK4dkukc5Y9kIZBI03CpDZC7bls5csrofuLo2RGX30PlLQ8HDhXACexavuftmQjiwMSbq9872gIZBKe2FHM4nZCEUCfttAkfi2jlBSt1NZBXulabrjTEBBfatb3JAZDZD"
-
-def sendMessage():#facebook_id):
-    facebook_id = '10153917348848395'
-    params = {"recipient": {"id": facebook_id},"message": {"text": "hello, world!"}}
-    r = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token=' + token, params=params)

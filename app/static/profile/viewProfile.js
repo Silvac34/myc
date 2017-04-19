f064db2cf6f08ec188d87cb626cf731f2d0d87a2
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-04-19 01:04:15 +0000

    https://trello.com/c/5T7tpmIB rajouter un autocomplete avec la liste des pays du monde

--- app/static/profile/viewProfile.js
+++ app/static/profile/viewProfile.js
@@ -132,9 +132,7 @@ angular.module('myApp.viewProfile', [ /*'google.places'*/ ])
     }
   };
 
-  /*$http.get("/static/sources/profile/countries.json").then(function(res){
-          $scope.countryList = res.data;                
-        });*/
-
-  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  $http.get("/static/sources/profile/countries.json").then(function(res){
+          $scope.countries = res.data;                
+        });
 }]);

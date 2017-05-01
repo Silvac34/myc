0dcb8f86e3bc58a6eee5997b940d2177c7cc1e13
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-05-01 06:40:14 +0000

    https://trello.com/c/ffit9Cql Connectez les filtres avec les marqueurs : changement complet dans la manière de faire apparaître les marqueurs, on utilise ngmap et non pas l'api de google maps qui créait des pb. Pareil pour les infowindow. Rajout de meal.mealPrice pour avoir un prix concordant avec celui proposé lors de l'inscription à un repas

--- app/static/viewMeals/viewMeals.js
+++ app/static/viewMeals/viewMeals.js
@@ -34,6 +34,27 @@ modViewMeals.controller('ViewMealsCtrl', ['$scope', '$state', '$uibModal', '$aut
     }
   });
 
+  function defineAllMealPrice() { //on définit le prix du repas qui doit s'afficher
+    for (var j = 0; j < $scope.meals.length; j++) {
+      $scope.meals[j].mealPrice = $scope.meals[j].price / $scope.meals[j].nbGuests; // de base c'est le nombre de participant diviser par le prix des courses (en dernier recours)
+      if ("cooks" in $scope.meals[j].detailedInfo.requiredGuests) {
+        if ($scope.meals[j].detailedInfo.requiredGuests.cooks.price != 0) {
+          $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cooks.price; //sinon c'est soit le prix d'aide cuisine
+        }
+      }
+      else if ("cleaners" in $scope.meals[j].detailedInfo.requiredGuests) {
+        if ($scope.meals[j].detailedInfo.requiredGuests.cleaners.price != 0) {
+          $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cleaners.price; //ou le prix aide vaisselle
+        }
+      }
+      else if ("simpleGuests" in $scope.meals[j].detailedInfo.requiredGuests) {
+        $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price; //enfin, s'il n'y a pas d'aide, c'est le prix invité
+      }
+    }
+  }
+
+  defineAllMealPrice();
+
   $scope.openModalDtld = function(meal_id) { //permet d'ouvrir les modals de chacun de repas associés
     for (var i = 0; i < $scope.meals.length; i++) {
       if ($scope.meals[i]._id == meal_id) {
@@ -189,14 +210,14 @@ modViewMeals.controller('ViewMealsCtrl', ['$scope', '$state', '$uibModal', '$aut
 
   $scope.priceRangeFilter = function(meal) {
     if ($scope.filter.priceFilterMin.value != null && $scope.filter.priceFilterMax.value != null) {
-      return ((meal.price / meal.nbGuests) >= $scope.filter.priceFilterMin.value && (meal.price / meal.nbGuests) <= $scope.filter.priceFilterMax.value);
+      return ($scope.meals[j].mealPrice >= $scope.filter.priceFilterMin.value && $scope.meals[j].mealPrice <= $scope.filter.priceFilterMax.value);
     }
     else {
       if ($scope.filter.priceFilterMin.value != null) {
-        return ((meal.price / meal.nbGuests) >= $scope.filter.priceFilterMin.value);
+        return ($scope.meals[j].mealPrice >= $scope.filter.priceFilterMin.value);
       }
       else if ($scope.filter.priceFilterMax.value != null) {
-        return ((meal.price / meal.nbGuests) <= $scope.filter.priceFilterMax.value);
+        return ($scope.meals[j].mealPrice <= $scope.filter.priceFilterMax.value);
       }
       else {
         return meal;
@@ -205,95 +226,13 @@ modViewMeals.controller('ViewMealsCtrl', ['$scope', '$state', '$uibModal', '$aut
   };
 
   $state.go("view_meals.mealsList");
-  $scope.googleMapsUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBwwM-TMFz42n8ZDaGHF-8rGt76cdoXN8M";
-
   NgMap.getMap("map").then(function(map) {
-    var markers = [];
-    for (var i = 0; i < $scope.meals.length; i++) {
-      var position = { // definit la position du repas
-        "lat": $scope.meals[i].address.lat,
-        "lng": $scope.meals[i].address.lng
-      };
-      markers[i] = new google.maps.Marker({ //créer le marqueur sur la carte
-        "position": position,
-        "map": map,
-        "title": "Menu: " + $scope.meals[i].menu.title,
-        "_id": $scope.meals[i]._id
-      });
-      markers[i].index = i;
-      var infoWindow = new google.maps.InfoWindow({
-        maxWidth: 250
-      }); //static infoWindow for all your markers
-      google.maps.event.addListener(markers[i], 'click', function() {
-        var meal_id = this._id;
-        for (var j = 0; j < $scope.meals.length; j++) {
-          if ($scope.meals[j]._id == meal_id) {
-            var mealPrice = $scope.meals[j].price / $scope.meals[j].ngGuests;
-            if ("cooks" in $scope.meals[j].detailedInfo.requiredGuests) {
-              mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cooks.price;
-            }
-            else if ("cleaners" in $scope.meals[j].detailedInfo.requiredGuests) {
-              mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cleaners.price;
-            }
-            else if ("simpleGuests" in $scope.meals[j].detailedInfo.requiredGuests) {
-              mealPrice = $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price;
-            }
-            var content = '<div id="content">' + //définit ce qui va apparaître dans la fenêtre lorsqu'on clique sur un marqueur
-              '<div class="cutlery-menu map-menu"><strong class="map-text-menu">' + $scope.meals[j].menu.title + '</strong></div>' +
-              '<div>' +
-              '<span class="date-calendar map-date">' + $filter('date')($scope.meals[j].time, 'EEEE dd MMMM') + '</span>' +
-              '<span class="time-calendar map-time">' + $filter('date')($scope.meals[j].time, 'HH:mm') + '</span>' +
-              '</div>' +
-              '<div class="clear10"></div>' +
-              '<span class="price-banknote map-price"><span style="color:initial;font-weight:100;">From</span> ' + $filter('currency')(mealPrice) + '</span>' +
-              '<div class="clear10"></div>' +
-              '<div>' +
-                '<div>' +
-                  '<strong>Hosted by:</strong>' +
-                '</div>' +
-                '<div style="float:right;"><a ui-sref="profile({userId: \'' + $scope.meals[j].admin._id + '\'})">' +
-                  '<span class="meal-admin-name">' + $scope.meals[j].admin.first_name + ' ' + $scope.meals[j].admin.last_name + '</span>' +
-                  '<img class="img-circle profilePic" style="margin-left: 10px;margin-bottom: 0px;" src="' + $scope.meals[j].admin.picture.data.url + '" alt="User profile picture" /></a>' +
-                '</div>' +
-                '<div class="clear10"></div>' +
-              '</div>' +
-              '<div class="text-center" style="width:110%">' +
-                '<a ng-if="' + $scope.meals[j].detailedInfo.subscribed + '==false" ng-click=openModalDtld(\'' + $scope.meals[j]._id + '\') class="btn btn-primary btn-sm">Subscribe</a>' +
-                '<a ng-if="' + $scope.meals[j].detailedInfo.subscribed + '==true" ng-click=openModalDtld(\'' + $scope.meals[j]._id + '\') class="btn btn-success btn-sm">See meal</a>' +
-              '</div>' +
-              '</div>';
-            var compiledContent = $compile(content)($scope);
-          }
-        }
-        infoWindow.setContent(compiledContent[0]);
-        infoWindow.open(map, markers[this.index]);
-        map.panTo(markers[this.index].getPosition());
-      });
-    }
+    $scope.map = map;
   });
 
-  /*console.log($scope.map);
-  var markers = [];
-  for (var i = 0; i < 8; i++) {
-    markers[i] = new google.maps.Marker({
-      title: "Hi marker " + i
-    })
-  }
-  $scope.GenerateMapMarkers = function() {
-    var numMarkers = Math.floor(Math.random() * 4) + 4; // betwween 4 & 8 of them
-    for (i = 0; i < numMarkers; i++) {
-      var lat = 1.280095 + (Math.random() / 100);
-      var lng = 103.850949 + (Math.random() / 100);
-      // You need to set markers according to google api instruction
-      // you don't need to learn ngMap, but you need to learn google map api v3
-      // https://developers.google.com/maps/documentation/javascript/marker
-      var latlng = new google.maps.LatLng(lat, lng);
-      markers[i].setPosition(latlng);
-      markers[i].setMap($scope.map)
-    }
+  $scope.openInfowindow = function() { //ouvre l'infowindow associé à la carte google
+    $scope.map.showInfoWindow(this.id, this.id); //1er argument = infowindow ID, 2eme argument = marker ID. Pour simplifier, j'ai attribué l'ID du repas aux deux.
   };
-
-  $scope.GenerateMapMarkers(); */
 
 }]);
 

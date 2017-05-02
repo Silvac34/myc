def30f44abda78328bed0c8164664cf6d6af8e23
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-05-02 10:24:58 +0000

    rajout des json et du nouveau service pour le moment non utilisé

---
+++ app/static/components/currencySymbolService.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = angular.module('currencySymbolService', []);
+
+app.factory('currencySymbolFactory', ['$http', '$q', function($http, $q) {
+    return function getCurrencySymbol(country_code) {
+        var myPromise = $q.defer();
+        return $http.get("/static/sources/createMeal/currency.json").then(function(result_currency) {
+            return $http.get("/static/sources/createMeal/currency_symbol.json").then(function(result_currency_symbol) {
+                var currency = result_currency.data[country_code];
+                myPromise.resolve(result_currency_symbol.data[currency].symbol_native);
+                return myPromise.promise;
+            });
+        });
+    };
+}]);

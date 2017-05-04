e8bae0b3d1dbcc7571648de70e493455ac732c6b
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-05-04 06:07:28 +0000

    https://trello.com/c/JJcuub6N mise en place du prix sur viewMyMeals

--- app/static/viewMyMeals/viewMyMeals.js
+++ app/static/viewMyMeals/viewMyMeals.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap', 'myApp.viewMyMealsDtld'])
+angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar', 'ui.bootstrap', 'myApp.viewMyMealsDtld', 'ngSanitize'])
 
 .controller('ViewMyMealsCtrl', ['$scope', 'response', '$uibModal', '$http', function($scope, response, $uibModal, $http) {
 
@@ -12,21 +12,36 @@ angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar
       $http.get("/static/sources/createMeal/currency_symbol.json").then(function(result_currency_symbol) {
         for (var j = 0; j < $scope.meals.length; j++) {
           for (var i = 0; i < $scope.meals[j].privateInfo.users.length; i++) {
-            var currency = result_currency.data[$scope.meals[j].address.country_code];
-            var currency_symbol = result_currency_symbol.data[currency].symbol_native;
+            var mealPrice = $scope.meals[j].price / $scope.meals[j].nbGuests; // de base c'est le nombre de participant diviser par le prix des courses (en dernier recours)
+            var priceUnit = Math.ceil($scope.meals[j].price / $scope.meals[j].nbGuests);
             if ($scope.meals[j].privateInfo.users[i]._id == userId) {
               var userRole = $scope.meals[j].privateInfo.users[i].role[0];
-              if (userRole == "simpleGuest") {
-                $scope.meals[j].priceUser = currency_symbol + " " + $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price;
+              if (userRole == "simpleGuests") {
+                mealPrice = $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price; //enfin, s'il n'y a pas d'aide, c'est le prix invité
               }
-              if (userRole == "admin") {
-                $scope.meals[j].priceUser = currency_symbol + " " + $scope.meals[j].detailedInfo.requiredGuests.hosts.price;
+              else {
+                if (userRole == "cooks") {
+                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cooks.price; //sinon c'est soit le prix d'aide cuisine
+                }
+                else if (userRole == "cleaners") {
+                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cleaners.price; //ou le prix aide vaisselle
+                }
+                else if (userRole == "admin") {
+                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.hosts.price; //ou le prix hôte
+                }
               }
-              if (userRole == "cook") {
-                $scope.meals[j].priceUser = currency_symbol + " " + $scope.meals[j].detailedInfo.requiredGuests.cooks.price;
+              var currency = result_currency.data[$scope.meals[j].address.country_code];
+              var currency_symbol = result_currency_symbol.data[currency].symbol_native;
+              var mealPriceWithSymbol = currency_symbol + " " + mealPrice;
+              var priceUnitWithSymbol = currency_symbol + " " + priceUnit;
+              if (mealPrice < priceUnit) {
+                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence hidden-xs">From</span> ' + mealPriceWithSymbol + '<span class="small color-text-priceSentence"> to</span> ' + priceUnitWithSymbol;
               }
-              if (userRole == "cleaner") {
-                $scope.meals[j].priceUser = currency_symbol + " " + $scope.meals[j].detailedInfo.requiredGuests.cleaners.price;
+              else if (mealPrice > priceUnit) {
+                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence hidden-xs">From</span> ' + priceUnitWithSymbol + '<span class="small color-text-priceSentence"> to</span> ' + mealPriceWithSymbol;
+              }
+              else if (mealPrice == priceUnit) {
+                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence">From</span> ' + priceUnitWithSymbol;
               }
             }
           }

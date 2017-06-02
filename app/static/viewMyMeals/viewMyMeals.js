71d8ff5ad597d30511480ededb95cac48f95fe2e
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-06-02 12:27:55 +0000

    https://trello.com/c/erEGJx0U rajout des notations sur viewMealDtld.html

--- app/static/viewMyMeals/viewMyMeals.js
+++ app/static/viewMyMeals/viewMyMeals.js
@@ -12,37 +12,22 @@ angular.module('myApp.viewMyMeals', ['ui.router', 'angular-svg-round-progressbar
       $http.get("/static/sources/createMeal/currency_symbol.json").then(function(result_currency_symbol) {
         for (var j = 0; j < $scope.meals.length; j++) {
           for (var i = 0; i < $scope.meals[j].privateInfo.users.length; i++) {
-            var mealPrice = $scope.meals[j].price / $scope.meals[j].nbGuests; // de base c'est le nombre de participant diviser par le prix des courses (en dernier recours)
-            var priceUnit = Math.ceil(10*$scope.meals[j].price / $scope.meals[j].nbGuests)/10;
             if ($scope.meals[j].privateInfo.users[i]._id == userId) {
               var userRole = $scope.meals[j].privateInfo.users[i].role[0];
               if (userRole == "simpleGuests") {
-                mealPrice = $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price; //enfin, s'il n'y a pas d'aide, c'est le prix invité
+                $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.simpleGuests.price; //enfin, s'il n'y a pas d'aide, c'est le prix invité
               }
-              else {
-                if (userRole == "cooks") {
-                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cooks.price; //sinon c'est soit le prix d'aide cuisine
-                }
-                else if (userRole == "cleaners") {
-                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cleaners.price; //ou le prix aide vaisselle
-                }
-                else if (userRole == "admin") {
-                  mealPrice = $scope.meals[j].detailedInfo.requiredGuests.hosts.price; //ou le prix hôte
-                }
+              if (userRole == "cooks") {
+                $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cooks.price; //sinon c'est soit le prix d'aide cuisine
+              }
+              if (userRole == "cleaners") {
+                $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.cleaners.price; //ou le prix aide vaisselle
+              }
+              if (userRole == "admin") {
+                $scope.meals[j].mealPrice = $scope.meals[j].detailedInfo.requiredGuests.hosts.price; //ou le prix hôte
               }
               var currency = result_currency.data[$scope.meals[j].address.country_code];
-              var currency_symbol = result_currency_symbol.data[currency].symbol_native;
-              var mealPriceWithSymbol = currency_symbol + " " + mealPrice;
-              var priceUnitWithSymbol = currency_symbol + " " + priceUnit;
-              if (mealPrice < priceUnit) {
-                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence hidden-xs">From</span> <strong>' + mealPriceWithSymbol + '</strong><span class="small color-text-priceSentence"> to</span> <strong>' + priceUnitWithSymbol + '</strong>';
-              }
-              else if (mealPrice > priceUnit) {
-                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence hidden-xs">From</span> <strong>' + priceUnitWithSymbol + '</strong><span class="small color-text-priceSentence"> to</span> <strong>' + mealPriceWithSymbol + '</strong>';
-              }
-              else if (mealPrice == priceUnit) {
-                $scope.meals[j].priceSentence = '<span class="small color-text-priceSentence">From</span> <strong>' + priceUnitWithSymbol;
-              }
+              $scope.meals[j].currency_symbol = result_currency_symbol.data[currency].symbol_native; ////récupère correctement la monnaie où s'effectue le repas
             }
           }
           $scope.meals[j].address.country = getCountry($scope.meals[j].address.country_code, res.data); //récupère correctement le pays

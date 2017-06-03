892b8c0991345bdfe93229ac2e3d42413e69b76c
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-06-03 01:25:20 +0000

    https://trello.com/c/erEGJx0U viewMyMeal.html (quand c'est en pending) pour l'affichage des notes

--- app/static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.js
+++ app/static/viewMyMeals/viewMyMealsDtld/viewMyMealsDtld.js
@@ -294,7 +294,9 @@ modMyMealsDetailed.controller('ViewMyMealsDtldCtrl', ['$scope', '$http', '$state
         }
     };
 
-
+    $scope.capitalizeFirstLetter = function(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
 
 }]);
 

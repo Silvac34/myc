58f07af13a381918d8c0ec36fc5027954cfca5f8
DimitriKohn <dimitri.kohn.pro@gmail.com>
2017-11-28 16:50:59 +0000

    Rajout de la navbar, fichier de traduction en anglais et transformation du css en scss

---
+++ myc/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  
+  constructor(private translate: TranslateService) {
+    let userLang = navigator.language; 
+    translate.setDefaultLang('en');
+    translate.use(userLang);
+  }
+  
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
+
+  
+  user = {
+    "_id": {
+        "$oid": "577946eb4403f92b93425e6a"
+    },
+    "privateInfo": {
+        "cellphone": "0669686766",
+        "preferences": {
+            "omnivorous_notification": true,
+            "veggies_notification": true,
+            "vegan_notification": true
+        },
+        "user_ref": "3229872073936",
+        "email": "dimitri.kohn@centrale-marseille.fr"
+    },
+    "picture": {
+        "data": {
+            "url": "https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/10574308_10154781062403395_8399053171141470099_n.jpg?oh=894f4bbf5fdc6f56612ca1845127824e&oe=5A6EF34D",
+            "is_silhouette": false
+        }
+    },
+    "first_name": "Dimitri",
+    "last_name": "Odyssound",
+    "gender": "male",
+    "facebook_id": "10153917348848395",
+    "link": "https://www.facebook.com/app_scoped_user_id/10153917348848395/",
+    "reviews": {
+        "positive": 1,
+        "negative": 1
+    },
+    "_updated": {
+        "$date": "2017-09-15T10:36:29.000Z"
+    },
+    "_etag": "793192952506211ba0fe4dae15180ae2e398b029"
+  }
+}
---
+++ myc/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { environment } from './../environments/environment';
+
+import { AppComponent } from './app.component';
+import { routes } from './app.router';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+// translation with ngx-translate
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+//router
+import { CreateMealComponent } from './create-meal/create-meal.component';
+import { ViewMealsComponent } from './view-meals/view-meals.component';
+import { MyMealsComponent } from './my-meals/my-meals.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateMealComponent,
+    ViewMealsComponent,
+    MyMealsComponent,
+    WelcomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule,
+    FormsModule,
+    routes,
+    NgbModule.forRoot(),
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

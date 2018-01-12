6e4d79d96c1d90087590740d0a80fcea72e14066
DimitriKohn <dimitri.kohn.pro@gmail.com>
2018-01-12 20:18:38 +0000

    Simplification et centralisation des css du footer, travail sur le form de createMeal

--- myc/src/app/create-meal/create-meal.component.ts
+++ myc/src/app/create-meal/create-meal.component.ts
@@ -1,55 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Meal } from './meal';
+//on importe le modèle de donnée pour ensuite pouvoir l'avoir comme variable pour le 2 way data binding
+// par la suite on utilisera import { Meal } from './meal', qui correspondra au modèle de donnée final
 @Component({
   selector: 'app-create-meal',
   templateUrl: './create-meal.component.html',
   styleUrls: ['./create-meal.component.scss']
 })
-export class CreateMealComponent implements OnInit {
-  editedMeal = {
-        "menu": {
-            "title": String,
-            "description": String
-        },
-        "price": Number,
-        "address": {
-            "town": String,
-            "country": String,
-            "lng": Number,
-            "postalCode": String,
-            "lat": Number
-        },
-        "privateInfo": {
-            "address": {
-                "lat": Number,
-                "utc_offset": Number,
-                "name": String,
-                "lng": Number
-            }
-        },
-        "detailedInfo": {
-            "requiredGuests": {
-                "cleaners": {
-                    "nbRquCleaners": Number,
-                    "timeCleaning": Date
-                },
-                "cooks": {
-                    "nbRquCooks": Number,
-                    "inputTimeCooking": Date
-                }
-            }
-        },
-        "automaticSubscription": Boolean,
-        "vegan": Boolean,
-        "kosher": Boolean,
-        "hallal": Boolean,
-        "veggies": Boolean,
-        "time": Date,
-        "currency_symbol": String
-    };
+
+export class CreateMealComponent {
     
-  constructor() { }
-
-  ngOnInit() { }
-
+   editedMeal : Meal = {
+      "menu": {
+        "title": "",
+        "description": ""
+      },
+      "price": null,
+      "address": {
+        "town": "",
+        "country": "",
+        "lng": null,
+        "postalCode": "",
+        "lat": null
+      },
+      "privateInfo": {
+        "address": {
+          "lat": null,
+          "utc_offset": null,
+          "name": "",
+          "lng": null
+        }
+      },
+      "detailedInfo": {
+        "requiredGuests": {
+          "cleaners": {
+            "nbRquCleaners": null,
+            "timeCleaning": new Date()
+          },
+          "cooks": {
+            "nbRquCooks": null,
+            "timeCooking": new Date()
+          },
+          "simpleGuests": {
+            "nbRquSimpleGuests": null
+          }
+        }
+      },
+      "automaticSubscription": false,
+      "vegan": false,
+      "kosher": false,
+      "hallal": false,
+      "veggies": false,
+      "time": new Date(),
+      "currency_symbol": ""
+    };
 }

e26916c13a0a8cf97d066998961d47df8997cc9f
DimitriKohn <dimitri.kohn.pro@gmail.com>
2018-05-13 08:52:29 +0000

    Commit

---
+++ app/static/config.js
@@ -0,0 +1,8 @@
+'use strict';
+// THIS FILE IS AUTO-GENERATED! EDITS               WILL NOT PERSIST!
+// Make edits in the appropriate JSON            file under ./config
+angular.module('config', [])
+
+.constant('ENV', {fbClientID:'1533396083620333',fbRedirectURI:'https://mycommuneaty.herokuapp.com/',appId:'1533396083620333',page_id:'254569051671689'})
+
+;
